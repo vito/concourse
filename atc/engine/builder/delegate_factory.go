ed32b2d2252a0efb41c8e5dacf8887abf5f003eb
Alex Suraci <suraci.alex@gmail.com>
2020-08-27 17:11:23 -0400

    atc: behavior: don't assume builds have jobs
    previously, many areas of the codebase - primary logging and tracing -
    assumed that builds had a job name.
    
    this commit centralizes the construction of lager.Data and tracing.Attrs
    for builds onto the db.Build type itself.
    
    it also modifies the step tracing flow to go through the step delegate.
    this is ultimately so that we can call the new .TracingAttrs method on
    the build, but also makes it a bit easier to test.
    
    Signed-off-by: Alex Suraci <suraci.alex@gmail.com>

--- atc/engine/builder/delegate_factory.go
+++ atc/engine/builder/delegate_factory.go
@@ -12,6 +12,7 @@ import (
 	"code.cloudfoundry.org/clock"
 	"code.cloudfoundry.org/lager"
 	"code.cloudfoundry.org/lager/lagerctx"
+	"go.opentelemetry.io/otel/api/trace"
 
 	"github.com/concourse/concourse/atc"
 	"github.com/concourse/concourse/atc/db"
@@ -19,6 +20,7 @@ import (
 	"github.com/concourse/concourse/atc/event"
 	"github.com/concourse/concourse/atc/exec"
 	"github.com/concourse/concourse/atc/runtime"
+	"github.com/concourse/concourse/tracing"
 	"github.com/concourse/concourse/vars"
 )
 
@@ -548,6 +550,19 @@ type buildStepDelegate struct {
 	buildVars *vars.BuildVariables
 	stderr    io.Writer
 	stdout    io.Writer
+}
+
+func (delegate *buildStepDelegate) StartSpan(
+	ctx context.Context,
+	component string,
+	extraAttrs tracing.Attrs,
+) (context.Context, trace.Span) {
+	attrs := delegate.build.TracingAttrs()
+	for k, v := range extraAttrs {
+		attrs[k] = v
+	}
+
+	return tracing.StartSpan(ctx, component, attrs)
 }
 
 func (delegate *buildStepDelegate) Variables() *vars.BuildVariables {
