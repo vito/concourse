5c9e303a9e2ec67eedb14460ddeea2e30abdd0d9
Alex Suraci <suraci.alex@gmail.com>
2020-08-27 12:10:04 -0400

    atc: behavior: clean up and godoc a few plans
    * rename update_resource/update_resource_type to just
      resource/resource_type for consistency with get/put plans
    * revise XXX comments and add godoc for Check/Get/Put/Task plans
    
    note: this is marked as behavior because it technically renames some
    plan fields, which would be an upgrade concern, but this code has never
    been released.
    
    Signed-off-by: Alex Suraci <suraci.alex@gmail.com>

--- atc/engine/builder/delegate_factory.go
+++ atc/engine/builder/delegate_factory.go
@@ -479,7 +479,7 @@ func (d *checkDelegate) pipeline() (db.Pipeline, error) {
 }
 
 func (d *checkDelegate) resource() (db.Resource, bool, error) {
-	if d.plan.UpdateResource == "" {
+	if d.plan.Resource == "" {
 		return nil, false, nil
 	}
 
@@ -492,13 +492,13 @@ func (d *checkDelegate) resource() (db.Resource, bool, error) {
 		return nil, false, err
 	}
 
-	resource, found, err := pipeline.Resource(d.plan.UpdateResource)
+	resource, found, err := pipeline.Resource(d.plan.Resource)
 	if err != nil {
 		return nil, false, fmt.Errorf("get pipeline resource: %w", err)
 	}
 
 	if !found {
-		return nil, false, fmt.Errorf("resource '%s' deleted", d.plan.UpdateResource)
+		return nil, false, fmt.Errorf("resource '%s' deleted", d.plan.Resource)
 	}
 
 	d.cachedResource = resource
@@ -507,7 +507,7 @@ func (d *checkDelegate) resource() (db.Resource, bool, error) {
 }
 
 func (d *checkDelegate) resourceType() (db.ResourceType, bool, error) {
-	if d.plan.UpdateResourceType == "" {
+	if d.plan.ResourceType == "" {
 		return nil, false, nil
 	}
 
@@ -520,13 +520,13 @@ func (d *checkDelegate) resourceType() (db.ResourceType, bool, error) {
 		return nil, false, err
 	}
 
-	resourceType, found, err := pipeline.ResourceType(d.plan.UpdateResourceType)
+	resourceType, found, err := pipeline.ResourceType(d.plan.ResourceType)
 	if err != nil {
 		return nil, false, fmt.Errorf("get pipeline resource type: %w", err)
 	}
 
 	if !found {
-		return nil, false, fmt.Errorf("resource type '%s' deleted", d.plan.UpdateResourceType)
+		return nil, false, fmt.Errorf("resource type '%s' deleted", d.plan.ResourceType)
 	}
 
 	d.cachedResourceType = resourceType
